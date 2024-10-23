53157ec112dbcdca9ab9e75bf5b2046fdaa4699c
livewebchat <erenkarakocw@gmail.com>
2024-10-23 17:13:51 +0300

    Webhook test

--- admin/src/components/Header/DropdownUser.tsx
+++ admin/src/components/Header/DropdownUser.tsx
@@ -14,7 +14,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Yönetici Hesabı
+            Yönetim Hesabı
           </span>
           <span className="hidden text-xs"></span>
         </span>
