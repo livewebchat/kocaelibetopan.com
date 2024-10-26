c466dd1dcb789847378f415d52bca9894035395b
Eren Karakoç <erenkarakoc@Erens-MacBook-Pro.local>
2024-10-26 21:37:26 +0300

    Frontend fixes

--- frontend/assets/js/vue/initializeSwipers.js
+++ frontend/assets/js/vue/initializeSwipers.js
@@ -10,4 +10,31 @@ const initializeSwipers = () => {
       prevEl: ".banner-prev-button",
     },
   })
+
+  const caseStudiesSlider = new Swiper(".case-studies-slider", {
+    loop: true,
+    slidesPerView: 3,
+    pagination: {
+      el: ".case-studies-pagination",
+      clickable: true,
+    },
+  })
+
+  const stepsSlider = new Swiper(".steps-slider", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".steps-pagination",
+      clickable: true,
+    },
+  })
+
+  const clientsSlider = new Swiper(".clients-slider", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".clients-pagination",
+      clickable: true,
+    },
+  })
 }
